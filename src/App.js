83613131dafee05381f7a4fff2999c62e8d95226
Amitaukande <96390843+Amitaukande@users.noreply.github.com>
2022-02-14 00:30:22 +0530

    Expense item Expenses app by using react js

--- src/App.js
+++ src/App.js
@@ -1,10 +1,49 @@
 import "./styles.css";
+// import ExpensesItem from "./components/ExpenseItem";
+import Expenses from "./components/Expenses";
 
-export default function App() {
+const App = () => {
+  let expense = [
+    {
+      id: "e1",
+      title: "School Fee",
+      amount: 700,
+      date: new Date(2021, 5, 12)
+    },
+    {
+      id: "e2",
+      title: "Books",
+      amount: 300,
+      date: new Date(2021, 5, 12)
+    },
+    {
+      id: "e3",
+      title: "House Rent",
+      amount: 600,
+      date: new Date(2021, 5, 12)
+    },
+    {
+      id: "e4",
+      title: "Collage",
+      amount: 200,
+      date: new Date(2021, 5, 12)
+    },
+    {
+      id: "e5",
+      title: "School Fee",
+      amount: 800,
+      date: new Date(2021, 5, 12)
+    }
+  ];
+  //  let expenseDate= new Date(2021, 3, 28);
+  //  let expenseTitle="School fee";
+  //  let expenseAmount=300;
+
   return (
-    <div className="App">
-      <h1>Hello CodeSandbox</h1>
-      <h2>Start editing to see some magic happen!</h2>
+    <div>
+      <h2>Let's get started</h2>
+      <Expenses item={expense} />
     </div>
   );
-}
+};
+export default App;
---
+++ src/components/ExpenseItem.js
@@ -0,0 +1,30 @@
+import "./ExpenseItem.css";
+
+import React, { useState } from "react";
+
+import ExpenseDate from "./ExpenseDate";
+
+const ExpenseItem = (props) => {
+  const [newTitle, setNewTitle] = useState("");
+
+  const [title, setTitle] = useState(props.title);
+
+  const clickHandler = () => {
+    setTitle(newTitle);
+  };
+  const changeHandler = (event) => {
+    setNewTitle(event.target.value);
+  };
+  return (
+    <div className="expense-item">
+      <ExpenseDate date={props.date} />
+      <div className="expense-item_description">
+        <h2>{title}</h2>
+        <div className="expense-item_price">${props.amount}</div>
+      </div>
+      <input type="text" value={newTitle} onChange={changeHandler} />
+      <button onClick={clickHandler}>Change Title</button>
+    </div>
+  );
+};
+export default ExpenseItem;
